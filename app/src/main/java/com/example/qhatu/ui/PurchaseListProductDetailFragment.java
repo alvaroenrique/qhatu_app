9de7d74b8c5b691d45564d0cdedca9e2956df507
Luis Rivera <luisriveradiaz99@gmail.com>
2020-07-08 18:00:42 -0500

    purchase list

---
+++ app/src/main/java/com/example/qhatu/ui/PurchaseListProductDetailFragment.java
@@ -0,0 +1,6 @@
+package com.example.qhatu.ui;
+
+import android.app.Fragment;
+
+public class PurchaseListProductDetailFragment extends Fragment {
+}
---
+++ app/src/main/java/com/example/qhatu/ui/PurchaseListProductFragment.java
@@ -0,0 +1,6 @@
+package com.example.qhatu.ui;
+
+import android.app.Fragment;
+
+public class PurchaseListProductFragment extends Fragment {
+}
