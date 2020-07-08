fc3fb2d094d6c828119721c18327cfa47117f88d
alvaroenrique <alvaroenriquehl@gmail.com>
2020-07-07 21:52:40 -0500

    Agregado layout inicial

---
+++ app/src/main/java/com/example/qhatu/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.qhatu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var listener: NavController.OnDestinationChangedListener
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.fragment)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView.setupWithNavController(navController)
+
+        navigationView.itemIconTintList = null
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.profileFragment,
+                R.id.currentPurchaseFragment,
+                R.id.purchaseHistoryFragment,
+                R.id.purchaseListFragment
+            ), drawerLayout
+        )
+
+    }
+
+    fun openCloseNavigationDrawer(view: View) {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
+}
